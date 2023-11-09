8de8780e42c5065790b78cb726733ec61cea8b85
vaanessamota <vanessaoliveira2706@gmail.com>
2023-11-09 12:48:28 -0300

    feat: get mobile battery level via ios platform

--- ios/Classes/MyBatteryPlugin.swift
+++ ios/Classes/MyBatteryPlugin.swift
@@ -1,19 +1,30 @@
 import Flutter
-import UIKit
 
 public class MyBatteryPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "my_battery_plugin", binaryMessenger: registrar.messenger())
-    let instance = MyBatteryPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "my_battery_plugin", binaryMessenger: registrar.messenger())
+        let instance = MyBatteryPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if call.method == "getBatteryLevel" {
+            let batteryLevel = getBatteryLevel()
+            result(batteryLevel)
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func getBatteryLevel() -> Int {
+        let device = UIDevice.current
+        device.isBatteryMonitoringEnabled = true
+
+        if device.batteryState == .unplugged {
+            return Int(device.batteryLevel * 100)
+        } else {
+            // The device is plugged in, so return -1 to indicate that it's charging.
+            return -1
+        }
+    }
 }
